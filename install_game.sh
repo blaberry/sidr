c43cea8b12b8ac23ebb2367ea1e6c1c2042835cf
Linn <hamstertejp@gmail.com>
2018-06-17 14:50:01 +0200

    Initial commit

---
+++ install_game.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+name = "neotokyo"
+appid = "313600"
+
+if [ $(docker volume ls -q | grep $name) == "" ] then
+  docker volume create $name
+fi
+docker run -v $name:/game steamcmd steamcmd @sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /game +app_update $appid validate +quit
