03ad1e07cdddcb473e1e991ff0f2af28911e8dae
Linn Engström <linn.engstroem@gmail.com>
2018-06-17 21:53:17 +0200

    run_game and install_game added

--- install_game.sh
+++ install_game.sh
@@ -6,4 +6,5 @@ appid = "313600"
 if [ $(docker volume ls -q | grep $name) == "" ] then
   docker volume create $name
 fi
+
 docker run -v $name:/game steamcmd steamcmd @sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /game +app_update $appid validate +quit
---
+++ run_game.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+name="neotokyo"
+appid="313600"
+maxplayers="12"
+if [ $(docker volume ls -q | grep $name) == "" ] then
+  echo "no docker volume called " + $name + " found."
+  exit 1
+fi
+
+docker run -P -v $name:/game $name -w /opt/$name --name "$(curl -L 'https://uzby.com/api.php?min=4&max=9')" \
+Xvfb :1 -screen 0 800x600x8 & \
+export DISPLAY=:1 \
+wine start srcds.exe -console -game NeotokyoSource -port 27015 +maxplayers $maxplayers +exec server.cfg +map nt_dawn_ctg
