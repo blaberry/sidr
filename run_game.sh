15eda565d42eab6cf52239fff47fc78b0ec9d73c
Linn Engström <linn.engstroem@gmail.com>
2018-06-17 23:42:52 +0200

    asd

--- run_game.sh
+++ run_game.sh
@@ -8,7 +8,8 @@ if [[ "$(docker volume ls -q | grep $name)" -eq "" ]]; then
   exit 1
 fi
 
-docker run -P -v "$name":/game "$name" -w /opt/"$name" --name "$(curl -L 'https://uzby.com/api.php?min=4&max=9')" \
-Xvfb :1 -screen 0 800x600x8 & \
-export DISPLAY=:1 \
-wine start srcds.exe -console -game NeotokyoSource -port 27015 +maxplayers "$maxplayers" +exec server.cfg +map nt_dawn_ctg
+if [[ $(docker image ls -q | grep $name) -eq "" ]]; then
+  docker build --tag $name $name
+fi
+
+docker run -P -v "$name":/game "$name" -w /opt/"$name" --name "$(curl -L 'https://uzby.com/api.php?min=4&max=9')" Xvfb :1 -screen 0 800x600x8 & export DISPLAY=:1 wine start srcds.exe -console -game NeotokyoSource -port 27015 +maxplayers "$maxplayers" +exec server.cfg +map nt_dawn_ctg
