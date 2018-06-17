083f4235343f7de23846959e2aadc9b77da7a163
Linn Engström <linn.engstroem@gmail.com>
2018-06-17 22:11:07 +0200

    fleshing out install.sh

--- install_game.sh
+++ install_game.sh
@@ -3,8 +3,16 @@
 name="neotokyo"
 appid="313600"
 
+# Create base server installation volume
 if [[ $(docker volume ls -q | grep $name) -eq "" ]]; then
   docker volume create $name
 fi
-
-docker run -v $name:/game steamcmd steamcmd "@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /game +app_update $appid validate +quit"
+# Build the steamCMD image if it doesn't exist, so we have something to run steamcmd inside of.
+if [[ $(docker image ls -q | grep steamcmd) -eq "" ]]; then
+  cd steamcmd
+  docker build
+fi
+# If building went well, let's install the game.
+if [[ $? -eq 0 ]]; then
+  docker run -v $name:/game steamcmd steamcmd "@sSteamCmdForcePlatformType windows +login anonymous +force_install_dir /game +app_update $appid validate +quit"
+fi
